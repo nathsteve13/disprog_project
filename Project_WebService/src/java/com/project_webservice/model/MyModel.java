62b59e69329aec1909b7668acc566d8e943a8641
nathsteve13 <nathanaelsteven06@gmail.com>
2024-07-10 22:11:58 +0700

    init webservice

---
+++ Project_WebService/src/java/com/project_webservice/model/MyModel.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project_webservice.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author natha
+ */
+public abstract class MyModel {
+    protected static Connection conn;
+    protected Statement statement;
+    protected ResultSet result;
+    
+    public MyModel() {
+        this.conn = this._getConnection();
+        this.statement = null;
+        this.result = null;
+    }
+    
+    private Connection _getConnection() {
+        if (MyModel.conn == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+                return DriverManager.getConnection("jdbc:mysql://localhost/dbreservation", "root", "");
+            } catch (Exception ex) {
+                System.out.println("error di getConnection" + ex.getMessage());
+            }
+        }
+        return MyModel.conn;
+    }
+    
+    public abstract void insertData();
+    public abstract void updateData();
+    public abstract void deleteData();
+    public abstract ArrayList<String> viewListData();
+}
---
+++ Project_WebService/src/java/com/project_webservice/model/Parking.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project_webservice.model;
+
+/**
+ *
+ * @author natha
+ */
+public class Parking {
+    
+}
