85f47d46ee8f84d162c5187f2e182ec8a4656454
nathsteve13 <nathanaelsteven06@gmail.com>
2024-07-10 23:23:06 +0700

    update acara

--- Project_WebService/src/java/com/project_webservice/model/Acara.java
+++ Project_WebService/src/java/com/project_webservice/model/Acara.java
@@ -4,10 +4,160 @@
  */
 package com.project_webservice.model;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 /**
  *
  * @author natha
  */
-public class Acara {
+public class Acara extends MyModel{
+    private int acara_id;
+    private String nama_acara;
+    private String deskripsi;
+    private Timestamp tanggal_acara;
+    private String lokasi_acara;
+    private int kapasitas;
+    private int harga_tiket;
+    private String status_acara;
     
+    public Acara(int acara_id, String nama_acara, String deskripsi, Timestamp tanggal_acara, String lokasi_acara, int kapasitas, int harga_tiket, String status_acara) {
+        this.acara_id = acara_id;
+        this.nama_acara = nama_acara;
+        this.deskripsi = deskripsi;
+        this.tanggal_acara = tanggal_acara;
+        this.lokasi_acara = lokasi_acara;
+        this.kapasitas = kapasitas;
+        this.harga_tiket = harga_tiket;
+        this.status_acara = status_acara;
+    }
+    
+    public Acara(String status_acara, int kapasitas, int acara_id) {
+        this.acara_id = acara_id;
+        this.status_acara = status_acara;
+        this.kapasitas = kapasitas;
+    }
+    public int getAcara_id() {
+        return acara_id;
+    }
+
+    public void setAcara_id(int acara_id) {
+        this.acara_id = acara_id;
+    }
+
+    public String getNama_acara() {
+        return nama_acara;
+    }
+
+    public void setNama_acara(String nama_acara) {
+        this.nama_acara = nama_acara;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public Timestamp getTanggal_acara() {
+        return tanggal_acara;
+    }
+
+    public void setTanggal_acara(Timestamp tanggal_acara) {
+        this.tanggal_acara = tanggal_acara;
+    }
+
+    public String getLokasi_acara() {
+        return lokasi_acara;
+    }
+
+    public void setLokasi_acara(String lokasi_acara) {
+        this.lokasi_acara = lokasi_acara;
+    }
+
+    public int getKapasitas() {
+        return kapasitas;
+    }
+
+    public void setKapasitas(int kapasitas) {
+        this.kapasitas = kapasitas;
+    }
+
+    public int getHarga_tiket() {
+        return harga_tiket;
+    }
+
+    public void setHarga_tiket(int harga_tiket) {
+        this.harga_tiket = harga_tiket;
+    }
+
+    public String getStatus_acara() {
+        return status_acara;
+    }
+
+    public void setStatus_acara(String status_acara) {
+        this.status_acara = status_acara;
+    }
+
+    @Override
+    public void insertData() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void updateData() {
+        try {
+            if (!MyModel.conn.isClosed()) {
+                PreparedStatement sql = MyModel.conn.prepareStatement(
+                        "UPDATE acara SET status_acara = ?, kapasitas = ? WHERE acara_id = ?");
+                sql.setString(1, this.status_acara);
+                sql.setInt(2, this.kapasitas);
+                sql.setInt(3, this.acara_id);
+                sql.executeUpdate();
+                sql.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteData() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<String> viewListData() {
+        ArrayList<String> hasil = new ArrayList<String>();
+        try {
+            this.statement = (Statement)MyModel.conn.createStatement();
+            this.result = this.statement.executeQuery("SELECT * FROM acara");
+            
+            while (this.result.next()) {
+                Acara a = new Acara(result.getInt("acara_id"), 
+                        result.getString("nama_acara"), 
+                        result.getString("deskripsi"),
+                        result.getTimestamp("tanggal_acara"),
+                        result.getString("lokasi_acara"),
+                        result.getInt("kapasitas"),
+                        result.getInt("harga_tiket"),
+                        result.getString("status_acara"));
+                hasil.add(a.acara_id + "%" + 
+                        a.nama_acara + "%" + 
+                        a.deskripsi + "%" + 
+                        a.tanggal_acara + "%" + 
+                        a.lokasi_acara + "%" + 
+                        a.kapasitas + "%" +
+                        a.harga_tiket + "%" +
+                        a.status_acara);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return hasil;
+    }
 }
--- Project_WebService/src/java/com/project_webservice/services/project_webservice.java
+++ Project_WebService/src/java/com/project_webservice/services/project_webservice.java
@@ -4,6 +4,7 @@
  */
 package com.project_webservice.services;
 
+import com.project_webservice.model.Acara;
 import com.project_webservice.model.User;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -21,14 +22,24 @@ public class project_webservice {
      */
     
     User u;
+    Acara a;
     @WebMethod(operationName = "checkLogin")
     public String checkLogin(@WebParam(name = "username") String username, @WebParam(name = "password") String password) {
         u = new User(username, password);
         return u.checkLogin();
     }
+    
     @WebMethod(operationName = "checkEmail")
     public boolean checkEmail(@WebParam(name = "email") String email) {
         u = new User(email);
         return u.checkEmail();
+    }
+    
+    @WebMethod(operationName = "updateDataAcara") 
+    public void updateDataAcara(@WebParam(name = "status_acara") String status_acara, 
+            @WebParam(name = "kapasitas") int kapasitas, 
+            @WebParam(name = "acara_id") int acara_id) {
+        a = new Acara(status_acara, kapasitas, acara_id);
+        a.updateData();
     }
 }
